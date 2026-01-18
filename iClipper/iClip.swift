557b1aa3c7eb0655f46e3e9808a08273214406a5
ABHIII777 <noob.ssooff@gmail.com>
2026-01-18 12:27:09 +0530

    First commit of iClipper

---
+++ iClipper/iClip.swift
@@ -0,0 +1,121 @@
+import SwiftUI
+import CoreData
+
+@MainActor
+
+class iClip: NSObject, NSApplicationDelegate {
+    
+    final class OverlayWindow: NSWindow {
+        override var canBecomeKey: Bool { true }
+        override var canBecomeMain: Bool { true }
+    }
+    
+    var overlayWindow: [NSWindow] = []
+    var wasHotKeyPressed = false
+    
+    var globalEventMonitor: Any?
+    var localEventMonitor: Any?
+    
+    func createOverlay() {
+        
+        overlayWindow.forEach{ $0.close() }
+        overlayWindow.removeAll()
+        
+        let windowSize = NSSize(width: 320, height: 100)
+        
+        guard let screen = NSScreen.main else { return }
+        let screenFrame = screen.visibleFrame
+        
+        let window = OverlayWindow(
+            contentRect: NSRect(
+                origin: CGPoint(
+                    x: screenFrame.midX - windowSize.width / 2,
+                    y: screenFrame.midY - windowSize.height / 2
+                ),
+                size: windowSize
+            ),
+            styleMask: [.borderless],
+            backing: .buffered,
+            defer: false
+        )
+        
+        window.level = .floating
+        window.isOpaque = false
+        window.backgroundColor = .clear
+        window.hasShadow = false
+        window.isReleasedWhenClosed = false
+        window.collectionBehavior = [.canJoinAllSpaces, .stationary, .fullScreenAuxiliary]
+        window.makeKeyAndOrderFront(nil)
+        window.contentView = NSHostingView(
+            rootView: ClipWindow()
+        )
+        
+        overlayWindow.append(window)
+    }
+    
+    func toggleOverlay() {
+        if self.overlayWindow.contains(where: {$0.isVisible}) {
+            self.overlayWindow.forEach{ $0.orderOut(nil) }
+            return
+        }
+        
+        self.createOverlay()
+        self.overlayWindow.forEach{ $0.makeKeyAndOrderFront(nil) }
+        NSApp.activate(ignoringOtherApps: true)
+    }
+    
+    func setUpHotKey() {
+        globalEventMonitor = NSEvent.addGlobalMonitorForEvents(matching: .flagsChanged) { [weak self] event in
+            Task{ @MainActor in self?.handleFlagsChanged(event)}
+        }
+        
+        localEventMonitor = NSEvent.addLocalMonitorForEvents(matching: .flagsChanged) { [weak self] event in
+            self?.handleFlagsChanged(event)
+            return event
+        }
+    }
+    
+    func handleFlagsChanged(_ event: NSEvent) {
+        let flags = event.modifierFlags.intersection(.deviceIndependentFlagsMask)
+        let isHotKeyPressed = flags.contains(.control) && flags.contains(.option)
+        
+        if isHotKeyPressed && !wasHotKeyPressed {
+            toggleOverlay()
+        }
+        
+        wasHotKeyPressed = isHotKeyPressed
+    }
+    
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        setUpHotKey()
+    }
+
+    func applicationWillTerminate(_ notification: Notification) {
+        if let global = globalEventMonitor {
+            NSEvent.removeMonitor(global)
+            globalEventMonitor = nil
+        }
+
+        if let local = localEventMonitor {
+            NSEvent.removeMonitor(local)
+            localEventMonitor = nil
+        }
+    }
+    
+    struct ClipWindow: View {
+        
+        @State private var query = ""
+        
+        var body: some View {
+            HStack(spacing: 12) {
+                TextField("Search in clipboard history...", text: $query)
+            }
+            .frame(width: 320, height: 100)
+            .background(
+                RoundedRectangle(cornerRadius: 12)
+                    .fill(.ultraThinMaterial)
+                    .cornerRadius(16)
+            )
+        }
+    }
+}
--- iClipper/iClipperApp.swift
+++ iClipper/iClipperApp.swift
@@ -1,21 +1,16 @@
-//
-//  iClipperApp.swift
-//  iClipper
-//
-//  Created by Abhi Patel on 18/01/26.
-//
-
 import SwiftUI
 import CoreData
 
 @main
 struct iClipperApp: App {
     let persistenceController = PersistenceController.shared
-
+    
+    @NSApplicationDelegateAdaptor(iClip.self)
+    var appDelegate: iClip
+    
     var body: some Scene {
-        WindowGroup {
-            ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        Settings {
+            EmptyView()
         }
     }
 }
